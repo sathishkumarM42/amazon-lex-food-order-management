f4531794ad99aef4dad7425b77643492355c713c
sathishkumarM42 <sathishkumar061292@gmail.com>
2021-06-13 10:25:08 +0530

    under single repo

--- TrackFood/index.js
+++ TrackFood/index.js
@@ -2,24 +2,20 @@
 const axios = require('axios');     
 exports.handler = (event, context, callback) => {
     var orderNumber=event.currentIntent.slots.orderNumber;
-    var reason=event.currentIntent.slots.reason;   
     var cred = process.env.credentials;
     var url = process.env.url;
 
     /* Construct request payload */
 
-    var reqdata='{	"orderNumber": "'+orderNumber+'",	"cancelReason": "'+reason+'"}';
-
-
-    let res = axios.post(url, reqdata,{headers : { Authorization:cred}}).then((res) => {
+     axios.get(url, {headers : { Authorization:cred}}).then((res) => {
               callback(null,{
 	                            "dialogAction": {
 		                                            "type": "Close",
 		                                            "fulfillmentState": "Fulfilled",
 		                                            "message": {
 			                                                      "contentType": "PlainText",
-			                                                      "content": "Thanks for contacting us, Your incedent is created with #"+res.data.CaseNumber+"." 
-                                                                }
+			                                                      "content": "your order is "+res.data.orderStatus
+	                                      	                  }
 	                                             }
                              });
       });
